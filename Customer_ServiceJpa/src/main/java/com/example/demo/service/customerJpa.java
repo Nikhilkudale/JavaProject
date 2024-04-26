5682bfe725d3537d7ed587010b16cfe535469453
Nikhilkudale <82772880+Nikhilkudale@users.noreply.github.com>
2024-04-26 17:13:26 +0530

    Delete Customer_ServiceJpa directory

--- Customer_ServiceJpa/src/main/java/com/example/demo/service/customerJpa.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-import com.example.demo.entity.customer;
-
-public interface customerJpa extends JpaRepositoryImplementation<customer, Integer> {
-
-}
